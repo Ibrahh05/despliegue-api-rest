3e1eca583a45fd13982f58e07ed1568c68e8525f
Ibrahh05 <7464122@alu.murciaeduca.es>
2025-10-23 00:35:41 +0200

    listado, adoptar y docker

---
+++ src/main/java/com/hellin/demo/controller/PetController.java
@@ -0,0 +1,53 @@
+package com.hellin.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import com.hellin.demo.entity.Pet;
+import com.hellin.demo.repository.PetRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+@RequestMapping("/pet")
+/**
+ * En este controlador se espone los enpoint referentes a Pets 
+ * @version 1.0
+ * @author Ibrahim Hanaoui Karbab
+ */
+public class PetController {
+
+
+    private PetRepository petRepository;
+
+    public PetController(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }  
+
+    /**
+     * Este method devuelve el listado de mascotas
+     * @return List<Pet> informacion de cada mascota
+     */
+
+    @GetMapping ("/list")
+    public List<Pet> hello() {
+        return petRepository.findAll();
+        
+    }
+
+    @PostMapping("/adopt/{id}")
+    public Pet adopt(@PathVariable long id) {
+        Pet pet = petRepository.findById(id).get();
+
+        pet.setAdopt(true);
+        return petRepository.save(pet);
+        
+        
+    }
+}
+
+
+
+
